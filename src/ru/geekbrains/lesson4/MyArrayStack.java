9f7a5cebbaa20f80d5bd80e09356b81691b645b5
uumarov <usman@umarov.ru>
2019-03-14 22:58:15 +0300

    lesson4 source

---
+++ src/ru/geekbrains/lesson4/MyArrayStack.java
@@ -0,0 +1,61 @@
+package ru.geekbrains.lesson4;
+
+import java.util.NoSuchElementException;
+
+public class MyArrayStack<Item> {
+    private Object[] stack = new Object[1];
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void resize(int capacity) {
+        Object[] temp = new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            temp[i] = stack[i];
+        }
+        stack = temp;
+    }
+
+    public void push(Item item) {
+        if (size == stack.length) {
+            resize(2 * stack.length);
+        }
+        stack[size++] = item;
+    }
+
+    public Item peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+
+        //unchecked
+        return (Item) stack[size - 1];
+    }
+
+    public Item pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+        Item item = (Item) stack[size - 1];
+        size--;
+        if (size == stack.length / 4 && size > 0) {
+            resize(stack.length / 2);
+        }
+        return item;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = size - 1; i >= 0 ; i--) {
+            s.append(((Item) stack[i]).toString());
+            s.append(", ");
+        }
+        return s.toString();
+    }
+}
---
+++ src/ru/geekbrains/lesson4/MyLinkedList.java
@@ -0,0 +1,233 @@
+package ru.geekbrains.lesson4;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyLinkedList<Item> implements Iterable<Item> {
+    @Override
+    public Iterator<Item> iterator() {
+        return new MyLinkedListIterator();
+    }
+
+    private class MyLinkedListIterator implements Iterator<Item> {
+
+        Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+        Node previous;
+
+        public Node(Node previous, Item item, Node next) {
+            this.previous = previous;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node first = null;
+    private Node last = null;
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Item getFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("List is empty");
+        }
+        return first.item;
+    }
+
+    public void addFirst(Item item) {
+        Node oldFirst = first;
+        first = new Node(null, item, oldFirst);
+        if (isEmpty()) {
+            last = first;
+        }
+        else {
+            oldFirst.previous = first;
+        }
+        size++;
+    }
+
+    public Item removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("List is empty");
+        }
+
+        Node second = first.next;
+        Item item = first.item;
+        first.item = null;
+        first.next = null;
+        first = second;
+        size--;
+        if (isEmpty()) {
+            last = null;
+        }
+        else {
+            second.previous = null;
+        }
+        return item;
+    }
+
+    public Item getLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("List is empty");
+        }
+        return last.item;
+    }
+
+    public void addLast(Item item) {
+        Node oldLast = last;
+        last = new Node(oldLast, item, null);
+        if (isEmpty()) {
+            first = last;
+        }
+        else {
+            oldLast.next = last;
+        }
+        size++;
+    }
+
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("List is empty");
+        }
+        Item item = last.item;
+        Node previous = last.previous;
+        last.previous = null;
+        last = previous;
+        size--;
+        if (isEmpty()) {
+            first = null;
+        }
+        else {
+            last.next = null;
+        }
+        return item;
+    }
+
+    public Item get(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        int currentIndex = 0;
+        Node current = first;
+        while (currentIndex < index) {
+            current = current.next;
+            currentIndex++;
+        }
+        return current.item;
+    }
+
+    public void set(int index, Item item) { //a[index] = item;
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        int currentIndex = 0;
+        Node current = first;
+        while (currentIndex < index) {
+            current = current.next;
+            currentIndex++;
+        }
+        current.item = item;
+    }
+
+    public int indexOf(Item item) {
+        Node current = first;
+        int currentIndex = 0;
+        while (current != null && !current.item.equals(item)) {
+            current = current.next;
+            currentIndex++;
+        }
+        return current != null ? currentIndex : -1;
+    }
+
+    public boolean contains(Item item) {
+        return indexOf(item) > -1;
+    }
+
+    public Item remove(Item item) {
+        Node current = first;
+
+        while (current != null && !current.item.equals(item)) {
+            current = current.next;
+        }
+        if (current == null) {
+            return null;
+        }
+        if (current == first) {
+            return removeFirst();
+        }
+        if (current == last) {
+            return removeLast();
+        }
+        Node next = current.next;
+        Node previous = current.previous;
+        previous.next = next;
+        next.previous = previous;
+        size--;
+        current.previous = null;
+        current.next = null;
+        return current.item;
+    }
+
+    public void add(int index, Item item) { //addBefore
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            addFirst(item);
+            return;
+        }
+        else if (index == size) {
+            addLast(item);
+            return;
+        }
+
+        int currentIndex = 0;
+        Node current = first;
+        while (currentIndex < index) {
+            current = current.next;
+            currentIndex++;
+        }
+        Node newNode = new Node(current.previous, item, current);
+        Node previous = current.previous;
+        previous.next = newNode;
+        current.previous = newNode;
+        size++;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        Node current = first;
+        while (current != null) {
+            s.append(current.item.toString());
+            s.append(", ");
+            current = current.next;
+        }
+        return s.toString();
+    }
+}
