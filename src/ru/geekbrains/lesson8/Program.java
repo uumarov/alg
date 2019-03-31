4328dae142e36c753fb3b6319924b0061628dd23
uumarov <usman@umarov.ru>
2019-04-01 00:47:46 +0300

    homework 8

--- src/ru/geekbrains/lesson8/Program.java
+++ src/ru/geekbrains/lesson8/Program.java
@@ -2,21 +2,21 @@ package ru.geekbrains.lesson8;
 
 public class Program {
     public static void main(String[] args) {
-//        MyChainingHashMap<Character, Integer> hm = new MyChainingHashMap<>();
-//        hm.put('a', 1);
-//        hm.put('b', 2);
-//        hm.put('x', 3);
-//        hm.put('t', 4);
-//        hm.put('u', 5);
-//        hm.put('p', 6);
-//        hm.put('q', 7);
-//        hm.put('z', 8);
-//        hm.put('g', 9);
-//
-//        System.out.println(hm.remove('z'));
-//        System.out.println(hm.remove('g'));
-//        System.out.println(hm.remove('a'));
-//        System.out.println(hm.get('a'));
+        MyChainingHashMap<Character, Integer> hm = new MyChainingHashMap<>();
+        hm.put('a', 1);
+        hm.put('b', 2);
+        hm.put('x', 3);
+        hm.put('t', 4);
+        hm.put('u', 5);
+        hm.put('p', 6);
+        hm.put('q', 7);
+        hm.put('z', 8);
+        hm.put('g', 9);
+
+        System.out.println(hm.remove('z'));
+        System.out.println(hm.remove('g'));
+        System.out.println(hm.remove('a'));
+        System.out.println(hm.get('a'));
 
         MyLinearProbbingHashMap<Character, Integer> lp = new MyLinearProbbingHashMap<>();
         lp.put('s', 1);
